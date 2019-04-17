6d135252b516bd3da1486f3276b8b3ae33cbe4ec
Andres Marcovecchio <andreshmarco@gmail.com>
2019-04-17 20:13:18 -0300

    Mover declaracion de librerias al .h

--- liss/lissandra.c
+++ liss/lissandra.c
@@ -8,8 +8,7 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "lissandra.h"
 
 int main(int argc,char** argv) {
