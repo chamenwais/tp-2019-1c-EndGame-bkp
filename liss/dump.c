510beba8817199e042f3e238dbf38963d2ce6b95
Andres Marcovecchio <andreshmarco@gmail.com>
2019-05-20 00:04:27 -0300

    Armando el dump

--- liss/dump.c
+++ liss/dump.c
@@ -10,8 +10,14 @@
 int dump(){
 
 	void dumpearTabla(void* nodo) {
-		!strcmp(((tp_nodoDeLaMemTable) nodo)->nombreDeLaTabla,nombreDeLaTabla);
-		}
+		//!strcmp(((tp_nodoDeLaMemTable) nodo)->nombreDeLaTabla,nombreDeLaTabla);
+		t_metadataDeLaTabla metadataDeLaTabla = obtenerMetadataDeLaTabla(((tp_nodoDeLaMemTable) nodo)->nombreDeLaTabla);
+		char* directorioDeLaTabla=string_new();
+		string_append(&directorioDeLaTabla, configuracionDelFS.puntoDeMontaje);
+		string_append(&directorioDeLaTabla, "/Tables/");
+		string_append(&directorioDeLaTabla, ((tp_nodoDeLaMemTable) nodo)->nombreDeLaTabla);
+
+	}
 
 	list_iterate(memTable,dumpearTabla);
 	vaciarMemTable();
