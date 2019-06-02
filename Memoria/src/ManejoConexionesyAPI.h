37886820dbae37d22281974b3a0b8eeeb25b08af
Francisco Romero <francisco.a.romero.5@gmail.com>
2019-06-02 11:43:16 -0300

    Se corrige devolución de SELECT al kernel

--- Memoria/src/ManejoConexionesyAPI.h
+++ Memoria/src/ManejoConexionesyAPI.h
@@ -12,11 +12,11 @@
 #include "Utilidades.h"
 #include "GestionSegPaginada.h"
 
-void realizar_select(char *, int);
+tp_select_rta_a_kernel realizar_select(char *, int);
 void realizar_insert(char *, long, uint16_t, char *);
 enum MENSAJES realizar_create(char *, char *, int, int);
 void loguear_value_por_pantalla(char *);
-tp_select_rta pedir_value_a_liss(char *, uint16_t);
+tp_select_rta_a_kernel pedir_value_a_liss(char *, uint16_t);
 void atender_create(int, int);
 void atender_select(int, int);
 void atender_insert(int, int);
@@ -29,5 +29,6 @@ void realizar_describe_para_tabla_particular(char *);
 void imprimir_informacion_tabla_particular(void *);
 enum MENSAJES realizar_drop(char *);
 void retornar_respuesta_al_kernel(enum MENSAJES, void(*enviador_respuesta_ok)(int), int);
+void convertir_respuesta_select(tp_select_rta_a_kernel,tp_select_rta, enum MENSAJES);
 
 #endif /* SRC_MANEJOCONEXIONESYAPI_H_ */
