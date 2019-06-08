d0db8090fa9bedf7cd4c0237f5af8847b8d8abb1
Francisco Romero <francisco.a.romero.5@gmail.com>
2019-06-08 13:05:26 -0300

    Se corrige uso de list_get con una func como param x list_find en Kernel

--- installer.sh
+++ installer.sh
@@ -6,17 +6,20 @@ sudo make clean
 sudo make all
 sudo make install
 cd ..
-sudo apt-get install libreadline6 libreadline6-dev
 cd COM
 make clean
 make all
 sudo make install
+sudo apt-get install libreadline6 libreadline6-dev
+sleep 15
+sudo apt-get install libpthread-stubs0-dev
+sleep 15
 cd tp-2019-1c-EndGame/liss/Debug
 make clean
 make all
 cd ../..
-sudo apt-get install libcunit1-dev libcunit1-doc libcunit1
-sleep 1m
+sudo apt-get install libcunit1-dev libcunit1
+sleep 15
 cd Memoria/Debug
 make clean
 make all
@@ -25,3 +28,7 @@ cd Kernel/Debug
 make clean
 make all
 cd ../..
+git clone https://github.com/sisoputnfrba/1C2019-Scripts-lql-checkpoint.git
+mkdir lissandra-checkpoint
+cp tp-2019-1c-EndGame/liss/Debug/Metadata lissandra-checkpoint
+./lanzadorProceso.sh
